20cb94078639af60c2c9df5978b128d2fb6a6c5f
Hannes Nisula <hannes@ducksquad.dev>
2024-08-08 20:52:54 +0200

    Add `getContext()` to assist reference resolver

--- src/main/kotlin/org/hannesnisula/cpp2plugin/psi/Cpp2PsiUtil.kt
+++ src/main/kotlin/org/hannesnisula/cpp2plugin/psi/Cpp2PsiUtil.kt
@@ -3,6 +3,10 @@ package org.hannesnisula.cpp2plugin.psi
 import com.intellij.openapi.util.TextRange
 import com.intellij.psi.PsiElement
 import com.intellij.psi.PsiReference
+import com.intellij.psi.util.PsiTreeUtil
+import org.hannesnisula.cpp2plugin.psi.impl.Cpp2FuncDeclImpl
+import org.hannesnisula.cpp2plugin.psi.impl.Cpp2NamespaceDeclImpl
+import org.hannesnisula.cpp2plugin.psi.impl.Cpp2TypeDeclImpl
 
 class Cpp2PsiUtil {
     companion object {
@@ -36,11 +40,22 @@ class Cpp2PsiUtil {
         fun getNameIdentifier(element: Cpp2Lvalue): PsiElement? {
             return element.node.findChildByType(Cpp2Types.IDENTIFIER_WORD)?.psi
         }
-        
+
         @JvmStatic
         fun getReferences(element: Cpp2Lvalue): Array<PsiReference> {
             val textRange = TextRange(0, element.textLength - 1)
             return arrayOf(Cpp2Reference(element.originalElement, textRange))
+        }
+
+        @JvmStatic
+        fun getContext(element: PsiElement): PsiElement? {
+            val parentScopeTypes = arrayOf(
+                Cpp2NamespaceDeclImpl::class.java,
+                Cpp2TypeDeclImpl::class.java,
+                Cpp2FuncDeclImpl::class.java,
+                Cpp2File::class.java
+            )
+            return PsiTreeUtil.getParentOfType(element, *parentScopeTypes)
         }
     }
 }
