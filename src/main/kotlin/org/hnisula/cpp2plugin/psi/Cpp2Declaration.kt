6973d076129c4595a77d7dab09c2ce977587d525
Hannes Nisula <hannes@ducksquad.dev>
2024-09-30 21:34:13 +0200

    WIP

---
+++ src/main/kotlin/org/hnisula/cpp2plugin/psi/Cpp2Declaration.kt
@@ -0,0 +1,5 @@
+package org.hnisula.cpp2plugin.psi
+
+import com.intellij.psi.PsiNamedElement
+
+interface Cpp2Declaration : PsiNamedElement
---
+++ src/main/kotlin/org/hnisula/cpp2plugin/psi/Cpp2TypeDeclMixin.kt
@@ -0,0 +1,5 @@
+package org.hnisula.cpp2plugin.psi
+
+import com.intellij.psi.PsiElement
+
+abstract class Cpp2TypeDeclMixin : Cpp2Declaration, PsiElement
